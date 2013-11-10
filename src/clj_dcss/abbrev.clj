a2bec5d577f9a7376c827a99355a5b1f58ae0a59
myfreeweb <floatboth@me.com>
2013-11-10 16:08:56 +0400

    add formicid

--- src/clj_dcss/abbrev.clj
+++ src/clj_dcss/abbrev.clj
@@ -14,6 +14,7 @@
    "Draconian"      "Dr"
    "Elf"            "El" ; Removed in 0.3
    "Felid"          "Fe"
+   "Formicid"       "Fo"
    "Gargoyle"       "Gr"
    "Ghoul"          "Gh"
    "Gnome"          "Gn" ; Removed in 0.6
